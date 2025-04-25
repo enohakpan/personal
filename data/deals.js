76a28db38a6392fa928ffe8c440d4496bef89d6d
enohakpan <dashdash59600@gmail.com>
2025-04-25 13:43:59 +0100

    images path checked

--- data/deals.js
+++ data/deals.js
@@ -5,16 +5,16 @@ export const deals = [
     image: "images/appliances-deals.png",
     title: "Appliances"
   }, {
-    image: "../images/discount.gif",
+    image: "images/discount.gif",
     title: "Up to 50% off"
   }, {
-    image: "../images/phones-and-tablets.png",
+    image: "images/phones-and-tablets.png",
     title: "Phones and Tablets"
   }, {
-    image: "../images/tv-and-audio.png",
+    image: "images/tv-and-audio.png",
     title: "TV and Audio Devices"
   }, {
-    image: "../images/up-to-80-off.png",
+    image: "images/up-to-80-off.png",
     title: "Up to 80% off"
   }];
 
