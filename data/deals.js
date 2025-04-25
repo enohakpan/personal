4b2e1eaddc17a9824994359be8bc7dc0fec271ea
enohakpan <dashdash59600@gmail.com>
2025-04-25 17:00:08 +0100

    test

--- data/deals.js
+++ data/deals.js
@@ -8,30 +8,30 @@ export const deals = [
     image: "/images/discount.gif",
     title: "Up to 50% off"
   }, {
-    image: "images/phones-and-tablets.png",
+    image: "/images/phones-and-tablets.png",
     title: "Phones and Tablets"
   }, {
-    image: "images/tv-and-audio.png",
+    image: "/images/tv-and-audio.png",
     title: "TV and Audio Devices"
   }, {
-    image: "images/up-to-80-off.png",
+    image: "/images/up-to-80-off.png",
     title: "Up to 80% off"
   }];
 
 
   export const advert = [{
-    image: "https://ng.jumia.is/cms/0-1-weekly-cps/0-2025/Global-mini-Campaigns/Week-15/Updated_Sliders/ECSF_712X384.png",
+    image: "/images/advert1.jpg",
     title: "Advert 1"
   }, {
-    image: "images/advert2.gif",
+    image: "/images/advert2.gif",
     title: "Advert 2"
   }, {
-    image: "images/advert3.gif",
+    image: "/images/advert3.gif",
     title: "Advert 3"
   }, {
-    image: "images/advert4.jpg",
+    image: "/images/advert4.jpg",
     title: "Advert 4"
   }, {
-    image: "images/advert5.png",
+    image: "/images/advert5.png",
     title: "Advert 5"
   }];
