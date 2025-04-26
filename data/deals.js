df6743875259b9755a232012ad6dbdda32ff886e
enohakpan <dashdash59600@gmail.com>
2025-04-26 09:45:31 +0100

    advert image url updated

--- data/deals.js
+++ data/deals.js
@@ -20,18 +20,18 @@ export const deals = [
 
 
   export const advert = [{
-    image: "/images/advert1.jpg",
+    image: "https://i.imgur.com/vx3alN2.jpeg",
     title: "Advert 1"
   }, {
-    image: "/images/advert2.gif",
+    image: "https://i.imgur.com/vwOwswo.gif",
     title: "Advert 2"
   }, {
-    image: "/images/advert3.gif",
+    image: "https://i.imgur.com/RELJVLg.gif",
     title: "Advert 3"
   }, {
-    image: "/images/advert4.jpg",
+    image: "https://i.imgur.com/TwkLjaT.jpeg",
     title: "Advert 4"
   }, {
-    image: "/images/advert5.png",
+    image: "https://i.imgur.com/GJ5LuGG.png",
     title: "Advert 5"
   }];
