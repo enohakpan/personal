92ba8b5f9c4b927ea07d3a34271d9b52a9bdd8f0
enohakpan <dashdash59600@gmail.com>
2025-04-26 09:57:06 +0100

    test. removed all data from deals.js

--- scripts/jumia.js
+++ scripts/jumia.js
@@ -1,4 +1,43 @@
-import { deals, advert } from "../data/deals.js";
+/* import { deals, advert } from "../data/deals.js";
+ */
+
+const deals = [
+  {image: "https://enohakpan.github.io/personal/images/awoof.png",
+    title: "Awoof"
+  }, {
+    image: "/images/appliances-deals.png",
+    title: "Appliances"
+  }, {
+    image: "/images/discount.gif",
+    title: "Up to 50% off"
+  }, {
+    image: "/images/phones-and-tablets.png",
+    title: "Phones and Tablets"
+  }, {
+    image: "/images/tv-and-audio.png",
+    title: "TV and Audio Devices"
+  }, {
+    image: "/images/up-to-80-off.png",
+    title: "Up to 80% off"
+  }];
+
+
+  const advert = [{
+    image: "images/advert1.jpg",
+    title: "Advert 1"
+  }, {
+    image: "images/advert2.gif",
+    title: "Advert 2"
+  }, {
+    image: "images/advert3.gif",
+    title: "Advert 3"
+  }, {
+    image: "images/advert4.jpg",
+    title: "Advert 4"
+  }, {
+    image: "images/advert5.png",
+    title: "Advert 5"
+  }];
 
 let dealHtml = "";
 let advertHtml = "";
